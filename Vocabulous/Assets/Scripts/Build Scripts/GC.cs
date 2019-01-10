330e4cee45b2ac264ac45cf28811c15c0627ab11
Max Bryans <maxsfw3@gmail.com>
2019-01-10 18:14:00 +0000

    Baby Steps

---
+++ Vocabulous/Assets/Scripts/Build Scripts/GC.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GC : MonoBehaviour
+{
+    [Header("This won't stay empty for long")]
+    public int forKickOff = 0;
+
+
+    // --------------------//
+    // establish Singelton //
+    // ------------------- //
+    public static GC Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    private static GC instance = null;
+    void Awake()
+    {
+        if (instance)
+        {
+            Debug.Log("Already a GameController - going to die now .....");
+            DestroyImmediate(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    //---------------------------//
+    // Finished Singelton set up //
+    // --------------------------//
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
