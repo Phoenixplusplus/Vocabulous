ca2f4d7f6fa396f8edb9b1134801675fd5ff3c79
Max Bryans <maxsfw3@gmail.com>
2019-01-21 23:42:37 +0000

    NEW dice ... GC major additions

---
+++ Vocabulous/Assets/Scripts/Build Scripts/OurAssets.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OurAssets : MonoBehaviour
+{
+    // Purely a Place holder for all pre-fabs, textures etc assets that might need to be called by other items
+    [Header("Old School Overlay Tile")]
+    public GameObject OLayTile;
+    [Header("Dice in format Dx")]
+    public GameObject Da;
+    public GameObject Db;
+    public GameObject Dc;
+    public GameObject Dd;
+    public GameObject De;
+    public GameObject Df;
+    public GameObject Dg;
+    public GameObject Dh;
+    public GameObject Di;
+    public GameObject Dj;
+    public GameObject Dk;
+    public GameObject Dl;
+    public GameObject Dm;
+    public GameObject Dn;
+    public GameObject Do;
+    public GameObject Dp;
+    public GameObject Dq;
+    public GameObject Dqu;
+    public GameObject Dr;
+    public GameObject Ds;
+    public GameObject Dt;
+    public GameObject Du;
+    public GameObject Dv;
+    public GameObject Dw;
+    public GameObject Dx;
+    public GameObject Dy;
+    public GameObject Dz;
+    [Header("Tiles in format Tx")]
+    public GameObject Ta;
+    public GameObject Tb;
+    public GameObject Tc;
+    public GameObject Td;
+    public GameObject Te;
+    public GameObject Tf;
+    public GameObject Tg;
+    public GameObject Th;
+    public GameObject Ti;
+    public GameObject Tj;
+    public GameObject Tk;
+    public GameObject Tl;
+    public GameObject Tm;
+    public GameObject Tn;
+    public GameObject To;
+    public GameObject Tp;
+    public GameObject Tq;
+    public GameObject Tqu;
+    public GameObject Tr;
+    public GameObject Ts;
+    public GameObject Tt;
+    public GameObject Tu;
+    public GameObject Tv;
+    public GameObject Tw;
+    public GameObject Tx;
+    public GameObject Ty;
+    public GameObject Tz;
+
+
+
+}
--- Vocabulous/Assets/Scripts/Max Playground/Player.cs
+++ Vocabulous/Assets/Scripts/Max Playground/Player.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 
 public struct PlayerStats
 {
+    int NewPlayer;          // flag RE if there is a new Player ... if 0 (or null) no Player, create a default and set to 1
     string name;
-    int WordDiceSize;
-    int WorSearchSize;
-    int AnagramMinLength;
+    float MusicVolume;      // 0 = Mute, 1 = full
+    float SFXVolume;        // 0 = Mute, 1 = full
+    int WordDiceSize;       // 4 = 4x4 (default), 5 = 5x5, 6 = 6x6
+    int WordSearchSize;     // 10 = 10x10, 15 = 15x15 (default), 20 = 20x20
+    int AnagramMinLength;   // 4,5,6 (Default),7,8
 }
 
 
@@ -21,6 +24,11 @@ public class PlayerManager
     }
 
     public void SavePlayer(ref PlayerStats player)
+    {
+
+    }
+
+    public void SetDefault(PlayerStats player)
     {
 
     }
