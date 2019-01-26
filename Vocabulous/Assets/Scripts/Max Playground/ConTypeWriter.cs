2f43b401e6d6d151a535442329b04d37f41b5fa6
Max Bryans <maxsfw3@gmail.com>
2019-01-26 13:50:27 +0000

    typewriter

---
+++ Vocabulous/Assets/Scripts/Max Playground/ConTypeWriter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConTypeWriter : MonoBehaviour
+{
+    [SerializeField]
+    private string myWord = "";
+    private GC gc;
+    private TypeKey[] myKeys;
+
+    // Code Time
+    // Q:7700, W:7701, E:7702, R:7703, T:7704, Y:7705, U:7706, I:7707, O:7708, P7709:
+    // A:7710, S:7711, D:7712, F:7713, G:7714, H:7715, J:7716, K:7717, L:7718
+    // Z:7719, X:7720, C:7721, V:7722, B:7723, N:7724, M:7725
+    // Space:7726, Back:7727, Find:7728
+
+    void Awake()
+    {
+        gc = GC.Instance;
+        if (gc != null) Debug.Log("TypeWriter Connected to GC");
+        myKeys = GetComponentsInChildren<TypeKey>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            TypeKey k = null;
+            foreach (TypeKey K in myKeys)
+            {
+                if (K.myHoverID == gc.NewHoverOver) k = K;
+            }
+            if (k != null)
+            {
+                k.press();
+                // check for back space
+                // check for find
+                // else add letter
+                myWord = myWord + k.myKey;
+            }
+        }
+    }
+}
---
+++ Vocabulous/Assets/Scripts/Max Playground/TypeKey.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TypeKey : MonoBehaviour
+{
+    private ConTypeWriter myCon;
+    private Tile_Controlller myTile;
+    private ConDice myDiceCon;
+    public string myKey;
+    public int myHoverID;
+    private bool animating = false;
+    private Vector3 OPosition;
+
+    public void press()
+    {
+        if (!animating) StartCoroutine("myPress");
+    }
+
+    IEnumerator myPress ()
+    {
+        animating = true;
+        bool dropping = true;
+        while (animating)
+        {
+            float rate = transform.localScale.y * 0.9f;
+            float bottom = OPosition.y - (transform.localScale.y * 0.2f);
+            if (dropping && transform.position.y > bottom)
+            {
+                transform.Translate(new Vector3(0, -rate * Time.deltaTime, 0));
+                if (transform.position.y <= bottom) dropping = false;
+            }
+            if (!dropping && transform.position.y < OPosition.y)
+            {
+                transform.Translate(new Vector3(0, rate * Time.deltaTime, 0));
+                if (transform.position.y <= OPosition.y)
+                {
+                    transform.position = OPosition;
+                    animating = false;
+                }
+            }
+        yield return null;
+        }
+    }
+
+
+
+    void Awake()
+    {
+        myCon = GetComponentInParent<ConTypeWriter>();
+        myDiceCon = GetComponentInChildren<ConDice>();
+        myDiceCon.ID = myHoverID;
+        myTile = GetComponentInChildren<Tile_Controlller>();
+        //myTile.setID(myHoverID);
+        OPosition = transform.position;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //press();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
