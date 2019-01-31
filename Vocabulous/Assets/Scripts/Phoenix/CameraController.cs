f66908fe59b849748a831e2e5eeea389a211c437
Phoenix <phoenixmee91@gmail.com>
2019-01-31 17:16:36 +0000

    CameraPanning++

--- Vocabulous/Assets/Scripts/Phoenix/CameraController.cs
+++ Vocabulous/Assets/Scripts/Phoenix/CameraController.cs
@@ -18,10 +18,46 @@ public class CameraController : MonoBehaviour
                 clickedWordDice, clickedWordSearch, clickedAnagram, clickedWordDrop, clickedSolver,
                 quitting;
     public Transform notInPlayTransform, wordDiceCameraTransform, wordSearchTransform, anagramTransform, wordDropTransform, solverTransform;
+    Vector3 wordDiceCameraTransformOrigin, wordSearchCameraTransformOrigin, anagramCameraTransformOrigin, wordDropCameraTransformOrigin, solverCameraTransformOrigin,
+            wordDiceUnrotatedForward, wordSearchUnrotatedForward, anagramUnrotatedForward, wordDropUnrotatedForward, solverUnrotatedForward;
 
     void Start()
     {
         gameController = GC.Instance;
+
+        // set origin positions for panning to go back to
+        wordDiceCameraTransformOrigin = wordDiceCameraTransform.position;
+        wordSearchCameraTransformOrigin = wordSearchTransform.position;
+        anagramCameraTransformOrigin = anagramTransform.position;
+        wordDropCameraTransformOrigin = wordDropTransform.position;
+        solverCameraTransformOrigin = solverTransform.position;
+
+        // set true forward direction for panning
+        // unrotate angle of original transform, grab its forward and assign, then rotate back (long winded but half decent way)
+        Vector3 wdt = wordDiceCameraTransform.transform.eulerAngles;
+        wordDiceCameraTransform.transform.eulerAngles = new Vector3(0, wordDiceCameraTransform.eulerAngles.y, wordDiceCameraTransform.eulerAngles.z);
+        wordDiceUnrotatedForward = wordDiceCameraTransform.forward;
+        wordDiceCameraTransform.transform.eulerAngles = wdt;
+
+        Vector3 wst = wordSearchTransform.transform.eulerAngles;
+        wordSearchTransform.transform.eulerAngles = new Vector3(0, wordSearchTransform.eulerAngles.y, wordSearchTransform.eulerAngles.z);
+        wordSearchUnrotatedForward = wordSearchTransform.forward;
+        wordSearchTransform.transform.eulerAngles = wst;
+
+        Vector3 at = anagramTransform.transform.eulerAngles;
+        anagramTransform.transform.eulerAngles = new Vector3(0, anagramTransform.eulerAngles.y, anagramTransform.eulerAngles.z);
+        anagramUnrotatedForward = anagramTransform.forward;
+        anagramTransform.transform.eulerAngles = at;
+
+        Vector3 wddt = wordDropTransform.transform.eulerAngles;
+        wordDropTransform.transform.eulerAngles = new Vector3(0, wordDropTransform.eulerAngles.y, wordDropTransform.eulerAngles.z);
+        wordDropUnrotatedForward = wordDropTransform.forward;
+        wordDropTransform.transform.eulerAngles = wddt;
+
+        Vector3 st = solverTransform.transform.eulerAngles;
+        solverTransform.transform.eulerAngles = new Vector3(0, solverTransform.eulerAngles.y, solverTransform.eulerAngles.z);
+        solverUnrotatedForward = solverTransform.forward;
+        solverTransform.transform.eulerAngles = st;
 
         transform.LookAt(cameraParent);
         targetAngle = new Vector3(0f, 20f, 0f);
@@ -40,7 +76,49 @@ public class CameraController : MonoBehaviour
         if (currentAngle.y < 0) currentAngle.y = 360f;
         if (currentAngle.y > 360f) currentAngle.y = 0f;
 
+        // out game rotation around table
         if (!inPlay) if (Input.GetMouseButton(0)) { targetAngle.y += (Input.GetAxis("Mouse X") * mouseSensitivty); }
+
+        // panning within game
+        if (inPlay)
+        {
+            if (Input.GetMouseButton(1))
+            {
+                if (playWordDice)
+                {
+                    wordDiceCameraTransform.position += wordDiceCameraTransform.right * (Input.GetAxis("Mouse X") * mouseSensitivty);
+                    wordDiceCameraTransform.position += wordDiceUnrotatedForward * (Input.GetAxis("Mouse Y") * mouseSensitivty);
+                }
+                if (playWordSearch)
+                {
+                    wordSearchTransform.position += wordSearchTransform.right * (Input.GetAxis("Mouse X") * mouseSensitivty);
+                    wordSearchTransform.position += wordSearchUnrotatedForward * (Input.GetAxis("Mouse Y") * mouseSensitivty);
+                }
+                if (playAnagram)
+                {
+                    anagramTransform.position += anagramTransform.right * (Input.GetAxis("Mouse X") * mouseSensitivty);
+                    anagramTransform.position += anagramUnrotatedForward * (Input.GetAxis("Mouse Y") * mouseSensitivty);
+                }
+                if (playWordDrop)
+                {
+                    wordDropTransform.position += wordDropTransform.right * (Input.GetAxis("Mouse X") * mouseSensitivty);
+                    wordDropTransform.position += wordDropUnrotatedForward * (Input.GetAxis("Mouse Y") * mouseSensitivty);
+                }
+                if (playSolver)
+                {
+                    solverTransform.position += solverTransform.right * (Input.GetAxis("Mouse X") * mouseSensitivty);
+                    solverTransform.position += solverUnrotatedForward * (Input.GetAxis("Mouse Y") * mouseSensitivty);
+                }
+            }
+            if (Input.GetMouseButtonUp(1))
+            {
+                if (playWordDice) { wordDiceCameraTransform.position = wordDiceCameraTransformOrigin; }
+                if (playWordSearch) { wordSearchTransform.position = wordSearchCameraTransformOrigin; }
+                if (playAnagram) { anagramTransform.position = anagramCameraTransformOrigin; }
+                if (playWordDrop) { wordDropTransform.position = wordDropCameraTransformOrigin; }
+                if (playSolver) { solverTransform.position = solverCameraTransformOrigin; }
+            }
+        }
 
         // for UIC to display play game button
         if (currentAngle.y > (gameController.RotTranWordDice.y + 360) - 20f && currentAngle.y < (gameController.RotTranWordDice.y + 360) + 20f && !inPlay) onWordDice = true;
@@ -150,7 +228,7 @@ public class CameraController : MonoBehaviour
         }
     }
 
-    // button click functions
+    // asset click functions / button click functions
     public void RotateToGameWordDice()
     {
         clickedWordDice = true;
